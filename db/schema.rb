0eb48e8d234f2ca4d399ec0779478221e95711ec
Stuart Sharpe <stuart.a.sharpe@gmail.com>
2017-07-01 18:49:15 -0700

    Controller clean up

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170627032749) do
+ActiveRecord::Schema.define(version: 20170702004007) do
+
+  create_table "pins", force: :cascade do |t|
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
